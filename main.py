224c397e65f838d4a129351e00a71a04473b96bc
Andrey Ivanenko <ivanenko0276@gmail.com>
2023-10-20 13:41:42 +0300

    test

---
+++ main.py
@@ -0,0 +1,19 @@
+import model
+import inference_mamdani
+import fuzzy_operators
+import streamlit as st
+
+crisp = [22, 185, 20, 50_000]
+inference_mamdani.preprocessing(model.input_lvs, model.output_lv)
+result = inference_mamdani.process(
+    model.input_lvs, model.output_lv, model.rule_base, crisp
+)
+
+print(result)
+
+for lv in model.input_lvs:
+    fuzzy_operators.draw_lv(lv)
+fuzzy_operators.draw_lv(model.output_lv)
+
+st.text("Відповідь:")
+st.text(result)
